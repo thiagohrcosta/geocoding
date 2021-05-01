3e99371d205edbd6d93c387db612ef52ffd8d8c5
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-30 23:34:18 -0300

    Fixes zoom

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,6 +1,12 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
@@ -9,7 +15,9 @@ const initMapbox = () => {
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
+
     });
+
 
   const markers = JSON.parse(mapElement.dataset.markers);
   markers.forEach((marker) => {
@@ -17,7 +25,11 @@ const initMapbox = () => {
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
   });
-  }
+
+  fitMapToMarkers(map, markers);
+
+  };
+
 };
 
 export { initMapbox };
